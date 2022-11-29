524ed90d9fce25809871f1609a72edd517f43813
wangmaoquan <1782976211@qq.com>
2022-11-29 15:31:06 +0800

    feat: add isFunction 方法

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -15,6 +15,8 @@ export const isIntegerKey = (key: unknown) =>
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === 'object';
 
+export const isFunction = (val: unknown): val is Function => typeof val === 'function'
+
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 export const hasOwn = (
   val: object,
