cba9a20459e16f0fc352ceae551825cad2e6f19d
wangmaoquan <1782976211@qq.com>
2022-12-01 17:12:59 +0800

    fix: 格式化代码

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,5 @@
 import { hasChanged, extend } from './../../shared/index';
+import { CollectionTypes } from './collectionHandlers';
 import {
   activeEffect,
   ReactiveEffect,
@@ -6,7 +7,13 @@ import {
   trackEffects,
   triggerEffects,
 } from './effect';
-import { isShallow, toRaw, toReactive, isReadonly, isReactive } from './reactive';
+import {
+  isShallow,
+  toRaw,
+  toReactive,
+  isReadonly,
+  isReactive,
+} from './reactive';
 
 declare const RefSymbol: unique symbol;
 export interface Ref<T = any> {
@@ -60,7 +67,12 @@ type UnwrapRef<T> = T extends Ref<infer V>
   : UnwrapRefSimple<T>;
 
 // 传入的T 是number | string | boolean | Ref 时直接返回T, 如果传入的是一个object 对每个键执行重复的判断
-export type UnwrapRefSimple<T> = T extends number | string | boolean | Ref
+export type UnwrapRefSimple<T> = T extends
+  | number
+  | string
+  | boolean
+  | Ref
+  | CollectionTypes
   ? T
   : T extends object
   ? {
@@ -74,36 +86,35 @@ export function isRef(r: any): r is Ref {
 }
 
 export function unref<T>(ref: Ref<T> | T): T {
-  return isRef(ref) ? ref.value : ref
+  return isRef(ref) ? ref.value : ref;
 }
 
 const proxyRefsHandlers: ProxyHandler<any> = {
   get(target, key, receiver) {
     // get的时候 如果是ref 返回 .value 否则返回本身 直接用unref 就行
-    return unref(Reflect.get(target, key, receiver))
+    return unref(Reflect.get(target, key, receiver));
   },
   set(target, key, value, receiver) {
     // 怎么set
     // 如果old是ref 且value 不是ref直接修改 oldvalue.value = value
     const oldValue = target[key];
-    if(isRef(oldValue) && !isRef(value)) {
+    if (isRef(oldValue) && !isRef(value)) {
       oldValue.value = value;
       return true;
     } else {
-      return Reflect.set(target, key, value, receiver)
+      return Reflect.set(target, key, value, receiver);
     }
-  }
-}
-
+  },
+};
 
 export type ShallowUnwrapRef<T> = {
-  [K in keyof T]: T[K] extends Ref<infer V> ? V : T[K] 
-}
+  [K in keyof T]: T[K] extends Ref<infer V> ? V : T[K];
+};
 
 /**
  * 对象中key 存在ref时 不用 obj.key.value 去访问 直接obj.key访问
- * @param obj 
+ * @param obj
  */
-export function proxyRefs<T extends object>(obj: T):ShallowUnwrapRef<T> {
-  return isReactive(obj) ? obj : new Proxy(obj, proxyRefsHandlers)
+export function proxyRefs<T extends object>(obj: T): ShallowUnwrapRef<T> {
+  return isReactive(obj) ? obj : new Proxy(obj, proxyRefsHandlers);
 }
