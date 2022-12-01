db81b968330dd66970b1229fcda0022c05d9b5fa
wangmaoquan <1782976211@qq.com>
2022-12-01 17:13:14 +0800

    faet: 增加三个方法

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -27,3 +27,8 @@ export const hasChanged = (value: any, oldValue: any): boolean =>
   !Object.is(value, oldValue);
 
 export const extend = Object.assign;
+
+export const objectToString = Object.prototype.toString
+export const toTypeString = (value: unknown): string =>
+  objectToString.call(value)
+export const toRawType = (value: unknown) => toTypeString(value).slice(8, -1)
