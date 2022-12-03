94855249360fa4d7c643a6c97e2ca00f9d83fec6
wangmaoquan <1782976211@qq.com>
2022-12-03 16:30:39 +0800

    fix: 增加测例

--- packages/reactivity/tests/readonly.spec.ts
+++ packages/reactivity/tests/readonly.spec.ts
@@ -1,5 +1,6 @@
-import { isProxy, isReactive, isReadonly } from './../src/reactive';
+import { isProxy, isReactive, isReadonly, reactive } from './../src/reactive';
 import { readonly } from '../src/reactive';
+import { effect } from '../src/effect';
 
 type Writable<T> = { -readonly [P in keyof T]: T[P] };
 describe('readonly', () => {
@@ -123,5 +124,137 @@ describe('readonly', () => {
       `Delete operation on key "Symbol(qux)" failed: target is readonly.`,
       // @ts-ignoreÍ
     ).toHaveBeenWarnedLast();
+  });
+
+  it('should not trigger effects', () => {
+    const wrapped: any = readonly({ a: 1 });
+    let dummy;
+    effect(() => {
+      dummy = wrapped.a;
+    });
+    expect(dummy).toBe(1);
+    wrapped.a = 2;
+    expect(wrapped.a).toBe(1);
+    expect(dummy).toBe(1);
+    // @ts-ignore
+    expect(`target is readonly`).toHaveBeenWarned();
+  });
+
+  describe('Array', () => {
+    it('should make nested values readonly', () => {
+      const original = [{ foo: 1 }];
+      const wrapped = readonly(original);
+      expect(wrapped).not.toBe(original);
+      expect(isProxy(wrapped)).toBe(true);
+      expect(isReactive(wrapped)).toBe(false);
+      expect(isReadonly(wrapped)).toBe(true);
+      expect(isReactive(original)).toBe(false);
+      expect(isReadonly(original)).toBe(false);
+      expect(isReactive(wrapped[0])).toBe(false);
+      expect(isReadonly(wrapped[0])).toBe(true);
+      expect(isReactive(original[0])).toBe(false);
+      expect(isReadonly(original[0])).toBe(false);
+      // get
+      expect(wrapped[0].foo).toBe(1);
+      // has
+      expect(0 in wrapped).toBe(true);
+      // ownKeys
+      expect(Object.keys(wrapped)).toEqual(['0']);
+    });
+
+    it('should not allow mutation', () => {
+      const wrapped: any = readonly([{ foo: 1 }]);
+      wrapped[0] = 1;
+      expect(wrapped[0]).not.toBe(1);
+      expect(
+        `Set operation on key "0" failed: target is readonly.`,
+        // @ts-ignore
+      ).toHaveBeenWarned();
+      wrapped[0].foo = 2;
+      expect(wrapped[0].foo).toBe(1);
+      expect(
+        `Set operation on key "foo" failed: target is readonly.`,
+        // @ts-ignore
+      ).toHaveBeenWarned();
+
+      // should block length mutation
+      wrapped.length = 0;
+      expect(wrapped.length).toBe(1);
+      expect(wrapped[0].foo).toBe(1);
+      expect(
+        `Set operation on key "length" failed: target is readonly.`,
+        // @ts-ignore
+      ).toHaveBeenWarned();
+
+      // mutation methods invoke set/length internally and thus are blocked as well
+      wrapped.push(2);
+      expect(wrapped.length).toBe(1);
+      // push triggers two warnings on [1] and .length
+      // @ts-ignore
+      expect(`target is readonly.`).toHaveBeenWarnedTimes(5);
+    });
+
+    it('should not trigger effects', () => {
+      const wrapped: any = readonly([{ a: 1 }]);
+      let dummy;
+      effect(() => {
+        dummy = wrapped[0].a;
+      });
+      expect(dummy).toBe(1);
+      wrapped[0].a = 2;
+      expect(wrapped[0].a).toBe(1);
+      expect(dummy).toBe(1);
+      // @ts-ignore
+      expect(`target is readonly`).toHaveBeenWarnedTimes(1);
+      wrapped[0] = { a: 2 };
+      expect(wrapped[0].a).toBe(1);
+      expect(dummy).toBe(1);
+      // @ts-ignore
+      expect(`target is readonly`).toHaveBeenWarnedTimes(2);
+    });
+  });
+
+  describe('readonly Map/weakMap', () => {
+    it('should make nested values readonly', () => {
+      const key1 = {};
+      const key2 = {};
+      // const original = new Map([
+      //   [key1, {}],
+      //   [key2, {}]
+      // ])
+      const original = new WeakMap([
+        [key1, {}],
+        [key2, {}],
+      ]);
+      const wrapped = readonly(original);
+      expect(wrapped).not.toBe(original);
+      expect(isProxy(wrapped)).toBe(true);
+      expect(isReactive(wrapped)).toBe(false);
+      expect(isReadonly(wrapped)).toBe(true);
+      expect(isReactive(original)).toBe(false);
+      expect(isReadonly(original)).toBe(false);
+      expect(isReactive(wrapped.get(key1))).toBe(false);
+      expect(isReadonly(wrapped.get(key1))).toBe(true);
+      expect(isReactive(original.get(key1))).toBe(false);
+      expect(isReadonly(original.get(key1))).toBe(false);
+    });
+
+    it('should not allow mutation & not trigger effect', () => {
+      const map: any = readonly(new Map());
+      // const map = readonly(new WeakMap());
+      const key = {};
+      let dummy;
+      effect(() => {
+        dummy = map.get(key);
+      });
+      expect(dummy).toBeUndefined();
+      map.set(key, 1);
+      expect(dummy).toBeUndefined();
+      expect(map.has(key)).toBe(false);
+      expect(
+        `Set operation on key "${key}" failed: target is readonly.`,
+        // @ts-ignore
+      ).toHaveBeenWarned();
+    });
   });
 });
