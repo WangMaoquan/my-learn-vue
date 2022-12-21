7d376e5331df3d1e5ae695a6e06f8b21dab1463e
wangmaoquan <1782976211@qq.com>
2022-12-21 11:07:04 +0800

    test: 增加effect测例

--- packages/reactivity/tests/effect.spec.ts
+++ packages/reactivity/tests/effect.spec.ts
@@ -36,7 +36,7 @@ describe('effect', () => {
   });
 
   it('scheduler', () => {
-    // 1. effect 除了传入的fn 还有第二个参数 
+    // 1. effect 除了传入的fn 还有第二个参数
     // 2. 当第二个参数传入中存在 scheduler时 第二次执行的就不会是effect的runner 而是 scheduler, 执行的时期在 trigger
     let dummy;
     let run: any;
@@ -68,138 +68,283 @@ describe('effect', () => {
   it('stop', () => {
     // effect 存在一个stop方法, 当执行stop 方法后 依赖发生变化 也不会执行当前这个 effect
     // 当重新执行runner时, 依赖发生变化 便会重新执行effect
-    let dummy
-    const obj = reactive({ prop: 1 })
+    let dummy;
+    const obj = reactive({ prop: 1 });
     const runner = effect(() => {
-      dummy = obj.prop
-    })
-    obj.prop = 2
-    expect(dummy).toBe(2)
-    stop(runner)
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
     // obj.prop = 3
-    // 执行obj.props++ 会先触发get 收集依赖可以理解为执行了runner, 
+    // 执行obj.props++ 会先触发get 收集依赖可以理解为执行了runner,
     // 但是已经stop得情况下 不应该再被收集
     obj.prop++;
-    expect(dummy).toBe(2)
+    expect(dummy).toBe(2);
     // stopped effect should still be manually callable
-    runner()
-    expect(dummy).toBe(3)
+    runner();
+    expect(dummy).toBe(3);
   });
 
   it('events: onStop', () => {
     // effect 第二个参数 存在onstop 时需要执行, 啥时候执行 => 执行stop 的时候
-    const onStop = jest.fn()
+    const onStop = jest.fn();
     const runner = effect(() => {}, {
-      onStop
-    })
+      onStop,
+    });
 
-    stop(runner)
-    expect(onStop).toHaveBeenCalled()
-  })
+    stop(runner);
+    expect(onStop).toHaveBeenCalled();
+  });
 
   it('should allow explicitly recursive raw function loops', () => {
-    const counter = reactive({ num: 0 })
+    const counter = reactive({ num: 0 });
     const numSpy = jest.fn(() => {
-      counter.num++
+      counter.num++;
       if (counter.num < 10) {
-        numSpy()
+        numSpy();
       }
-    })
-    effect(numSpy)
-    expect(counter.num).toEqual(10)
-    expect(numSpy).toHaveBeenCalledTimes(10)
-  })
+    });
+    effect(numSpy);
+    expect(counter.num).toEqual(10);
+    expect(numSpy).toHaveBeenCalledTimes(10);
+  });
 
   it('should avoid infinite loops with other effects', () => {
-    const nums = reactive({ num1: 0, num2: 1 })
+    const nums = reactive({ num1: 0, num2: 1 });
 
-    const spy1 = jest.fn(() => (nums.num1 = nums.num2))
-    const spy2 = jest.fn(() => (nums.num2 = nums.num1))
-    effect(spy1)
-    effect(spy2)
-    expect(nums.num1).toBe(1)
-    expect(nums.num2).toBe(1)
-    expect(spy1).toHaveBeenCalledTimes(1)
-    expect(spy2).toHaveBeenCalledTimes(1)
-    nums.num2 = 4
-    expect(nums.num1).toBe(4)
-    expect(nums.num2).toBe(4)
-    expect(spy1).toHaveBeenCalledTimes(2)
-    expect(spy2).toHaveBeenCalledTimes(2)
-    nums.num1 = 10
-    expect(nums.num1).toBe(10)
-    expect(nums.num2).toBe(10)
-    expect(spy1).toHaveBeenCalledTimes(3)
-    expect(spy2).toHaveBeenCalledTimes(3)
-  })
+    const spy1 = jest.fn(() => (nums.num1 = nums.num2));
+    const spy2 = jest.fn(() => (nums.num2 = nums.num1));
+    effect(spy1);
+    effect(spy2);
+    expect(nums.num1).toBe(1);
+    expect(nums.num2).toBe(1);
+    expect(spy1).toHaveBeenCalledTimes(1);
+    expect(spy2).toHaveBeenCalledTimes(1);
+    nums.num2 = 4;
+    expect(nums.num1).toBe(4);
+    expect(nums.num2).toBe(4);
+    expect(spy1).toHaveBeenCalledTimes(2);
+    expect(spy2).toHaveBeenCalledTimes(2);
+    nums.num1 = 10;
+    expect(nums.num1).toBe(10);
+    expect(nums.num2).toBe(10);
+    expect(spy1).toHaveBeenCalledTimes(3);
+    expect(spy2).toHaveBeenCalledTimes(3);
+  });
 
   it('should observe has operations', () => {
-    let dummy
-    const obj = reactive<{ prop?: string | number }>({ prop: 'value' })
-    effect(() => (dummy = 'prop' in obj))
+    let dummy;
+    const obj = reactive<{ prop?: string | number }>({ prop: 'value' });
+    effect(() => (dummy = 'prop' in obj));
 
-    expect(dummy).toBe(true)
-    delete obj.prop
-    expect(dummy).toBe(false)
-    obj.prop = 12
-    expect(dummy).toBe(true)
-  })
+    expect(dummy).toBe(true);
+    delete obj.prop;
+    expect(dummy).toBe(false);
+    obj.prop = 12;
+    expect(dummy).toBe(true);
+  });
 
   it('should observe properties on the prototype chain', () => {
-    let dummy
-    const counter = reactive<{ num?: number }>({ num: 0 })
-    const parentCounter = reactive({ num: 2 })
-    Object.setPrototypeOf(counter, parentCounter)
-    effect(() => (dummy = counter.num))
+    let dummy;
+    const counter = reactive<{ num?: number }>({ num: 0 });
+    const parentCounter = reactive({ num: 2 });
+    Object.setPrototypeOf(counter, parentCounter);
+    effect(() => (dummy = counter.num));
 
-    expect(dummy).toBe(0)
-    delete counter.num
-    expect(dummy).toBe(2)
-    parentCounter.num = 4
-    expect(dummy).toBe(4)
-    counter.num = 3
-    expect(dummy).toBe(3)
-  })
+    expect(dummy).toBe(0);
+    delete counter.num;
+    expect(dummy).toBe(2);
+    parentCounter.num = 4;
+    expect(dummy).toBe(4);
+    counter.num = 3;
+    expect(dummy).toBe(3);
+  });
 
   it('should observe has operations on the prototype chain', () => {
-    let dummy
-    const counter = reactive<{ num?: number }>({ num: 0 })
-    const parentCounter = reactive<{ num?: number }>({ num: 2 })
-    Object.setPrototypeOf(counter, parentCounter)
-    effect(() => (dummy = 'num' in counter))
+    let dummy;
+    const counter = reactive<{ num?: number }>({ num: 0 });
+    const parentCounter = reactive<{ num?: number }>({ num: 2 });
+    Object.setPrototypeOf(counter, parentCounter);
+    effect(() => (dummy = 'num' in counter));
 
-    expect(dummy).toBe(true)
-    delete counter.num
-    expect(dummy).toBe(true)
-    delete parentCounter.num
-    expect(dummy).toBe(false)
-    counter.num = 3
-    expect(dummy).toBe(true)
-  })
+    expect(dummy).toBe(true);
+    delete counter.num;
+    expect(dummy).toBe(true);
+    delete parentCounter.num;
+    expect(dummy).toBe(false);
+    counter.num = 3;
+    expect(dummy).toBe(true);
+  });
 
   it('should observe inherited property accessors', () => {
-    let dummy, parentDummy, hiddenValue: any
-    const obj = reactive<{ prop?: number }>({})
+    let dummy, parentDummy, hiddenValue: any;
+    const obj = reactive<{ prop?: number }>({});
     const parent = reactive({
       set prop(value) {
-        hiddenValue = value
+        hiddenValue = value;
       },
       get prop() {
-        return hiddenValue
-      }
-    })
-    Object.setPrototypeOf(obj, parent)
-    effect(() => (dummy = obj.prop))
-    effect(() => (parentDummy = parent.prop))
+        return hiddenValue;
+      },
+    });
+    Object.setPrototypeOf(obj, parent);
+    effect(() => (dummy = obj.prop));
+    effect(() => (parentDummy = parent.prop));
 
-    expect(dummy).toBe(undefined)
-    expect(parentDummy).toBe(undefined)
-    obj.prop = 4
-    expect(dummy).toBe(4)
+    expect(dummy).toBe(undefined);
+    expect(parentDummy).toBe(undefined);
+    obj.prop = 4;
+    expect(dummy).toBe(4);
     // this doesn't work, should it?
     // expect(parentDummy).toBe(4)
-    parent.prop = 2
-    expect(dummy).toBe(2)
-    expect(parentDummy).toBe(2)
-  })
+    parent.prop = 2;
+    expect(dummy).toBe(2);
+    expect(parentDummy).toBe(2);
+  });
+
+  it('should run the passed function once (wrapped by a effect)', () => {
+    const fnSpy = jest.fn(() => {});
+    effect(fnSpy);
+    expect(fnSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should observe basic properties', () => {
+    let dummy;
+    const counter = reactive({ num: 0 });
+    effect(() => (dummy = counter.num));
+
+    expect(dummy).toBe(0);
+    counter.num = 7;
+    expect(dummy).toBe(7);
+  });
+
+  it('should observe multiple properties', () => {
+    let dummy;
+    const counter = reactive({ num1: 0, num2: 0 });
+    effect(() => (dummy = counter.num1 + counter.num1 + counter.num2));
+
+    expect(dummy).toBe(0);
+    counter.num1 = counter.num2 = 7;
+    expect(dummy).toBe(21);
+  });
+
+  it('should handle multiple effects', () => {
+    let dummy1, dummy2;
+    const counter = reactive({ num: 0 });
+    effect(() => (dummy1 = counter.num));
+    effect(() => (dummy2 = counter.num));
+
+    expect(dummy1).toBe(0);
+    expect(dummy2).toBe(0);
+    counter.num++;
+    expect(dummy1).toBe(1);
+    expect(dummy2).toBe(1);
+  });
+
+  it('should observe nested properties', () => {
+    let dummy;
+    const counter = reactive({ nested: { num: 0 } });
+    effect(() => (dummy = counter.nested.num));
+
+    expect(dummy).toBe(0);
+    counter.nested.num = 8;
+    expect(dummy).toBe(8);
+  });
+
+  it('should observe delete operations', () => {
+    let dummy;
+    const obj = reactive<{
+      prop?: string;
+    }>({ prop: 'value' });
+    effect(() => (dummy = obj.prop));
+
+    expect(dummy).toBe('value');
+    delete obj.prop;
+    expect(dummy).toBe(undefined);
+  });
+
+  it('should observe has operations', () => {
+    let dummy;
+    const obj = reactive<{ prop?: string | number }>({ prop: 'value' });
+    effect(() => (dummy = 'prop' in obj));
+
+    expect(dummy).toBe(true);
+    delete obj.prop;
+    expect(dummy).toBe(false);
+    obj.prop = 12;
+    expect(dummy).toBe(true);
+  });
+
+  it('should observe properties on the prototype chain', () => {
+    let dummy;
+    const counter = reactive<{ num?: number }>({ num: 0 });
+    const parentCounter = reactive({ num: 2 });
+    Object.setPrototypeOf(counter, parentCounter);
+    effect(() => (dummy = counter.num));
+
+    expect(dummy).toBe(0);
+    delete counter.num;
+    expect(dummy).toBe(2);
+    parentCounter.num = 4;
+    expect(dummy).toBe(4);
+    counter.num = 3;
+    expect(dummy).toBe(3);
+  });
+
+  it('should observe has operations on the prototype chain', () => {
+    let dummy;
+    const counter = reactive<{ num?: number }>({ num: 0 });
+    const parentCounter = reactive<{ num?: number }>({ num: 2 });
+    Object.setPrototypeOf(counter, parentCounter);
+    effect(() => (dummy = 'num' in counter));
+
+    expect(dummy).toBe(true);
+    delete counter.num;
+    expect(dummy).toBe(true);
+    delete parentCounter.num;
+    expect(dummy).toBe(false);
+    counter.num = 3;
+    expect(dummy).toBe(true);
+  });
+
+  it('should observe inherited property accessors', () => {
+    let dummy, parentDummy, hiddenValue: any;
+    const obj = reactive<{ prop?: number }>({});
+    const parent = reactive({
+      set prop(value) {
+        hiddenValue = value;
+      },
+      get prop() {
+        return hiddenValue;
+      },
+    });
+    Object.setPrototypeOf(obj, parent);
+    effect(() => (dummy = obj.prop));
+    effect(() => (parentDummy = parent.prop));
+
+    expect(dummy).toBe(undefined);
+    expect(parentDummy).toBe(undefined);
+    obj.prop = 4;
+    expect(dummy).toBe(4);
+    // this doesn't work, should it?
+    // expect(parentDummy).toBe(4)
+    parent.prop = 2;
+    expect(dummy).toBe(2);
+    expect(parentDummy).toBe(2);
+  });
+
+  it('should observe function call chains', () => {
+    let dummy;
+    const counter = reactive({ num: 0 });
+    effect(() => (dummy = getNum()));
+
+    function getNum() {
+      return counter.num;
+    }
+
+    expect(dummy).toBe(0);
+    counter.num = 2;
+    expect(dummy).toBe(2);
+  });
 });
