03b970c471ad46fbe2147f529a77c8fdbc04a51c
wangmaoquan <1782976211@qq.com>
2022-12-25 11:32:22 +0800

    chore: 跑测例时增加对 tsconfig中配置的文件别名的解析

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  setupFilesAfterEnv: ['./scripts/setupJestEnv.ts'],
-  globals: {
-    __DEV__: true,
-  },
-  rootDir: __dirname,
-}
+	setupFilesAfterEnv: ['./scripts/setupJestEnv.ts'],
+	globals: {
+		__DEV__: true
+	},
+	rootDir: __dirname,
+	watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+	moduleNameMapper: {
+		'^@vue/(.*?)$': '<rootDir>/packages/$1/src'
+	},
+	testMatch: ['<rootDir>/packages/**/tests/**/*spec.[jt]s?(x)'],
+	testPathIgnorePatterns: process.env.SKIP_E2E
+		? // ignore example tests on netlify builds since they don't contribute
+		  // to coverage and can cause netlify builds to fail
+		  ['/node_modules/', '/examples/tests']
+		: ['/node_modules/']
+};
