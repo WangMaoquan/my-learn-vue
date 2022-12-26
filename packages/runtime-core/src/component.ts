cb07d70ff73be293b47815de6ecafec8b3bff2f5
wangmaoquan <1782976211@qq.com>
2022-12-26 15:27:53 +0800

    feat: 实现classComponent

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -1,4 +1,27 @@
+import { isFunction } from '@vue/shared';
+import { EmitsOptions } from './componentEmits';
+import { ComponentOptions } from './componentOptions';
+import { ComponentPublicInstance } from './componentPublicInstance';
+
 export type Data = Record<string, unknown>;
 export interface Component {}
 export type ConcreteComponent = {};
 export interface ComponentInternalInstance {}
+
+export interface ClassComponent {
+	new (...args: any[]): ComponentPublicInstance<any, any, any, any, any>;
+	__vccOpts: ComponentOptions;
+}
+
+export type SetupContext<E = EmitsOptions> = E extends any
+	? {
+			attrs: Data;
+			slots: {};
+			emit: Function;
+			expose: (exposed?: Record<string, any>) => void;
+	  }
+	: never;
+
+export function isClassComponent(value: unknown): value is ClassComponent {
+	return isFunction(value) && '__vccOpts' in value;
+}
