bf680623b94402cb2f8244bd49b48986be59fc23
wangmaoquan <1782976211@qq.com>
2022-12-26 15:28:32 +0800

    feat: vue2.x this的类型实现

--- packages/runtime-core/src/componentPublicInstance.ts
+++ packages/runtime-core/src/componentPublicInstance.ts
@@ -1,1 +1,47 @@
-export type ComponentPublicInstance = {};
+import { EmitsOptions } from './componentEmits';
+import {
+	ComponentInjectOptions,
+	ComponentOptionsBase,
+	ComponentOptionsMixin,
+	ComputedOptions,
+	MethodOptions
+} from './componentOptions';
+
+export type ComponentPublicInstance<
+	P = {}, // props
+	B = {}, // setup
+	D = {}, // data
+	C extends ComputedOptions = {}, // computed
+	M extends MethodOptions = {}, // methods
+	E extends EmitsOptions = {}, // emit 属性
+	PublicProps = P,
+	Defaults = {},
+	MakeDefaultsOptional extends boolean = false,
+	Options = ComponentOptionsBase<any, any, any, any, any, any, any, any, any>, // component的基本属性
+	I extends ComponentInjectOptions = {} // inject
+> = {};
+
+export type CreateComponentPublicInstance<
+	P = {},
+	B = {},
+	D = {},
+	C extends ComputedOptions = {},
+	M extends MethodOptions = {},
+	Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
+	Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
+	E extends EmitsOptions = {},
+	Defaults = {},
+	I extends ComponentInjectOptions = {}
+> = ComponentPublicInstance<
+	P,
+	B,
+	D,
+	C,
+	M,
+	E,
+	P,
+	{},
+	false,
+	ComponentOptionsBase<P, B, D, C, M, Mixin, Extends, E, string, Defaults>,
+	I
+>;
