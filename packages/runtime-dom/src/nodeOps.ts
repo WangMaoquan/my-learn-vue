9020015d2537c349f4f3196c821e75f512c373b2
wangmaoquan <1782976211@qq.com>
2022-12-26 14:04:13 +0800

    feat: 实现insert, remove, createElement方法

--- packages/runtime-dom/src/nodeOps.ts
+++ packages/runtime-dom/src/nodeOps.ts
@@ -1,6 +1,26 @@
 import { RendererOptions } from '@vue/runtime-core';
 
+// svg namespace
+export const svgNS = 'http://www.w3.org/2000/svg';
+
+// 获取的document对象
+const doc = (typeof document !== 'undefined' ? document : null) as Document;
+
 export const nodeOps: RendererOptions<Node, Element> = {
-	hostE: 'test1' as any,
-	host: 'test2' as any
+	insert(child, parent, anchor) {
+		parent.insertBefore(child, anchor || null);
+	},
+	remove(child) {
+		const parent = child.parentNode;
+		if (parent) {
+			parent.removeChild(child);
+		}
+	},
+	createElement(tag, isSVG, is, props) {
+		const el = isSVG
+			? doc.createElementNS(svgNS, tag)
+			: doc.createElement(tag, is ? { is } : undefined);
+
+		return el;
+	}
 };
