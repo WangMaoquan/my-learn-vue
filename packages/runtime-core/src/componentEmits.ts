596d87451919a9cac5c2d44ddf360571f6c1001d
wangmaoquan <1782976211@qq.com>
2022-12-28 11:57:49 +0800

    feat: 增加定义实现的emit

--- packages/runtime-core/src/componentEmits.ts
+++ packages/runtime-core/src/componentEmits.ts
@@ -1,6 +1,24 @@
+import { camelize, EMPTY_OBJ, toHandlerKey } from '@vue/shared';
+import { ComponentInternalInstance } from './component';
+
 export type ObjectEmitsOptions = Record<
 	string,
 	((...args: any[]) => any) | null
 >;
 
 export type EmitsOptions = ObjectEmitsOptions | string[];
+
+export function emit(
+	instance: ComponentInternalInstance,
+	event: string,
+	...rawArgs: any[]
+) {
+	if (instance.isUnmounted) return;
+	const props = instance.vnode.props || EMPTY_OBJ;
+	let handlerName;
+	let handler =
+		props[(handlerName = toHandlerKey(event))] ||
+		props[(handlerName = toHandlerKey(camelize(event)))];
+
+	handler(...rawArgs);
+}
