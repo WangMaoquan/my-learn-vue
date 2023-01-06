505414f783e0da1a969d15a72862a0c54daf585c
wangmaoquan <1782976211@qq.com>
2023-01-06 14:50:42 +0800

    feat: 增加设置当前Renderinstance

--- packages/runtime-core/src/componentRenderContext.ts
+++ packages/runtime-core/src/componentRenderContext.ts
@@ -2,3 +2,11 @@ import { ComponentInternalInstance } from './component';
 
 export let currentRenderingInstance: ComponentInternalInstance | null = null;
 export let currentScopeId: string | null = null;
+
+export function setCurrentRenderingInstance(
+	instance: ComponentInternalInstance | null
+): ComponentInternalInstance | null {
+	const prev = currentRenderingInstance;
+	currentRenderingInstance = instance;
+	return prev;
+}
