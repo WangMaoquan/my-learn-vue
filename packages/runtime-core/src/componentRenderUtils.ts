22b1fe60651c3cbdc08e2c5aab53fae725abcaaa
wangmaoquan <1782976211@qq.com>
2023-01-06 14:51:31 +0800

    feat: 执行render 的方法

--- packages/runtime-core/src/componentRenderUtils.ts
+++ packages/runtime-core/src/componentRenderUtils.ts
@@ -1,3 +1,8 @@
+import { ShapeFlags } from '@vue/shared';
+import { ComponentInternalInstance } from './component';
+import { setCurrentRenderingInstance } from './componentRenderContext';
+import { createVNode, normalizeVNode, VNode } from './vnode';
+
 // $attr 在render是否被使用的标志
 let accessedAttrs = false;
 
@@ -5,5 +10,51 @@ export function markAttrsAccessed() {
 	accessedAttrs = true;
 }
 
-// 校验通过
-accessedAttrs;
+export function renderComponentRoot(
+	instance: ComponentInternalInstance
+): VNode {
+	const {
+		vnode,
+		proxy,
+		props,
+		attrs,
+		render,
+		renderCache,
+		data,
+		setupState,
+		ctx
+	} = instance;
+
+	let result: any;
+	let fallthroughAttrs;
+	// 保存上一次的 instance
+	const prev = setCurrentRenderingInstance(instance);
+	if (__DEV__) {
+		accessedAttrs = false;
+	}
+
+	try {
+		// 执行 状态组件的render
+		if (vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+			const proxyToUse = proxy;
+			result = normalizeVNode(
+				render!.call(
+					proxyToUse,
+					proxyToUse!,
+					renderCache,
+					props,
+					setupState,
+					data,
+					ctx
+				)
+			);
+			fallthroughAttrs = attrs;
+		}
+	} catch (err) {
+		console.log(err);
+		result = createVNode(Comment);
+	}
+
+	setCurrentRenderingInstance(prev);
+	return result!;
+}
