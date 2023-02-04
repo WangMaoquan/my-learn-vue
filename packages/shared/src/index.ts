95d546bcc9f9092e382363464749f71ad422a3ae
wangmaoquan <1782976211@qq.com>
2023-02-04 15:09:58 +0800

    feat: 增加 EMPTY_ARR

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -14,6 +14,8 @@ export const EMPTY_OBJ: { readonly [key: string]: any } = __DEV__
 	? Object.freeze({})
 	: {};
 
+export const EMPTY_ARR = __DEV__ ? Object.freeze([]) : [];
+
 export const isArray = Array.isArray;
 
 export const isString = (val: unknown): val is string =>
