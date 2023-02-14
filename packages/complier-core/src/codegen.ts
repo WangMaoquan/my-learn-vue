5cb327df8086f5ea58458488a40e421244e37811
wangmaoquan <1782976211@qq.com>
2023-02-14 13:41:50 +0800

    fix: 简单实现generate

--- packages/complier-core/src/codegen.ts
+++ packages/complier-core/src/codegen.ts
@@ -1,1 +1,24 @@
-export {};
+import { RootNode } from './ast';
+
+// https://template-explorer.vuejs.org/
+
+export interface CodegenResult {
+	code: string;
+	ast: RootNode;
+}
+
+export function generate(ast: RootNode): CodegenResult {
+	let code = `return `;
+	const functionName = 'render';
+	const args = ['_ctx', '_cache'];
+	const signature = args.join(', ');
+
+	code += `function ${functionName}(${signature}){`;
+	code += `return "hi"`;
+	code += `}`;
+
+	return {
+		code,
+		ast
+	};
+}
--- packages/complier-core/tests/codegen.spec.ts
+++ packages/complier-core/tests/codegen.spec.ts
@@ -1,1 +1,11 @@
-describe('codegen', () => {});
+import { baseParse, generate } from '../src';
+
+describe('codegen', () => {
+	test('simple', () => {
+		const ast = baseParse('hi');
+		const { code } = generate(ast);
+
+		// 使用快照 -u更新快照
+		expect(code).toMatchSnapshot();
+	});
+});
