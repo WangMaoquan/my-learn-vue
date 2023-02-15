8b48286816c0b7f71e23608cc6156f4ff730188b
wangmaoquan <1782976211@qq.com>
2023-02-15 11:26:17 +0800

    fix: runtime-core包导出

--- packages/runtime-core/src/index.ts
+++ packages/runtime-core/src/index.ts
@@ -1,8 +1,145 @@
-export * from './apiCreateApp';
-export { getCurrentInstance, registerRuntimeCompiler } from './component';
-export * from './renderer';
-export { createVNode, createElementVNode } from './vnode';
-export { defineComponent } from './apiDefineComponent';
-export { inject, provide } from './apiInject';
+export {
+	reactive,
+	ref,
+	readonly,
+	unref,
+	proxyRefs,
+	isRef,
+	toRef,
+	toRefs,
+	isProxy,
+	isReactive,
+	isReadonly,
+	isShallow,
+	customRef,
+	triggerRef,
+	shallowRef,
+	shallowReactive,
+	shallowReadonly,
+	markRaw,
+	toRaw,
+	effect,
+	stop,
+	ReactiveEffect
+} from '@vue/reactivity';
+export { computed } from './apiComputed';
 export { watch, watchEffect } from './apiWatch';
+export { provide, inject } from './apiInject';
+export { nextTick } from './scheduler';
+export { defineComponent } from './apiDefineComponent';
+
+export { getCurrentInstance } from './component';
+
 export { h } from './h';
+
+export { createVNode, cloneVNode, isVNode } from './vnode';
+
+export { Fragment, Text, Comment, Static } from './vnode';
+
+export { createRenderer } from './renderer';
+export { queuePostFlushCb } from './scheduler';
+export {
+	handleError,
+	callWithErrorHandling,
+	callWithAsyncErrorHandling,
+	ErrorCodes
+} from './errorHandling';
+
+export { registerRuntimeCompiler } from './component';
+
+export { TrackOpTypes, TriggerOpTypes } from '@vue/reactivity';
+export type {
+	Ref,
+	ToRef,
+	ToRefs,
+	UnwrapRef,
+	ShallowRef,
+	ShallowUnwrapRef,
+	CustomRefFactory,
+	ReactiveFlags,
+	DeepReadonly,
+	ShallowReactive,
+	UnwrapNestedRefs,
+	ComputedRef,
+	WritableComputedRef,
+	WritableComputedOptions,
+	ComputedGetter,
+	ComputedSetter,
+	ReactiveEffectRunner,
+	ReactiveEffectOptions,
+	EffectScheduler
+} from '@vue/reactivity';
+export type {
+	WatchEffect,
+	WatchOptions,
+	WatchOptionsBase,
+	WatchCallback,
+	WatchSource,
+	WatchStopHandle
+} from './apiWatch';
+export type { InjectionKey } from './apiInject';
+export type {
+	App,
+	AppConfig,
+	AppContext,
+	Plugin,
+	CreateAppFunction
+} from './apiCreateApp';
+export type {
+	VNode,
+	VNodeChild,
+	VNodeTypes,
+	VNodeProps,
+	VNodeArrayChildren,
+	VNodeNormalizedChildren
+} from './vnode';
+export type {
+	Component,
+	ConcreteComponent,
+	ComponentInternalInstance,
+	SetupContext,
+	ComponentCustomProps,
+	AllowedComponentProps
+} from './component';
+export type { DefineComponent } from './apiDefineComponent';
+export type {
+	ComponentOptions,
+	ComponentOptionsMixin,
+	ComponentOptionsWithoutProps,
+	ComponentOptionsWithArrayProps,
+	ComponentCustomOptions,
+	ComponentOptionsBase,
+	ComponentProvideOptions,
+	RenderFunction,
+	MethodOptions,
+	ComputedOptions,
+	ComponentInjectOptions
+} from './componentOptions';
+export type { EmitsOptions, ObjectEmitsOptions } from './componentEmits';
+export type {
+	ComponentPublicInstance,
+	CreateComponentPublicInstance
+} from './componentPublicInstance';
+export type {
+	Renderer,
+	RendererNode,
+	RendererElement,
+	RendererOptions,
+	RootRenderFunction
+} from './renderer';
+export type { Slot, Slots } from './componentSlots';
+export type {
+	Prop,
+	PropType,
+	ComponentPropsOptions,
+	ExtractPropTypes
+} from './componentProps';
+export type { Directive } from './directives';
+
+export { createElementVNode } from './vnode';
+export {
+	toDisplayString,
+	camelize,
+	capitalize,
+	toHandlerKey
+} from '@vue/shared';
